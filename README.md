357923694880bb3f9adf4f4027c74e63a6a3f752
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2024-04-09 16:25:16 +0300

    simple quote page. manipulating the dom

---
+++ README.md
@@ -0,0 +1,16 @@
+Quote Generator(vanilla.JS)
+
+The purpose of this project was to manipulate the dom and create a simple dynamic page to hold a quote and display the author separately without using a object to hold the information.
+
+
+
+step 1 make two arrays to hold the information i want to display 
+
+step 2 select the divs i wanted to manipulate in the html by the id name.
+
+step 3 add an event listener to the page so when it is clicked a function can be run
+
+step 4 assign a function to randomly choose a number to use this value to select a quote 
+
+step 5 create a function to take the random number value and use it to get information from the arrays to append their content to the designated spot in the html
+
---
+++ index.js
@@ -0,0 +1,33 @@
+//quotes
+const array = ["quote one longer longer ois good its even longerrrr oh wow ita crazyyyy noooo this is lorem  lorem also long so i can swee how it looks in a long box that i have created oh wow its long", "so is quote two","so is three","and five","and more","exapmle text", "we will continue","and on and on it goes"]
+const authors = ["man1","man2",'Man3',"man4","man5","man6","man7","man8","man9", "man10"]
+
+// select the div in the html by the id name 
+const button = document.getElementById("quoteBox")
+const holder = document.getElementById("holder")
+const author = document.getElementById("author")
+const body = document.getElementById("body")
+
+
+//when body is clicked change quote
+body.addEventListener("click", () => {
+    changeQuote()
+})
+
+// use random number to select text from the 
+// arrays and append them to the html
+function changeQuote () {
+let randomChoice = getRandomQuote()
+holder.textContent = `"${array[randomChoice]}"`
+author.textContent = `-${authors[randomChoice]}`
+return randomChoice
+}
+
+//get random number
+function getRandomQuote() {
+    let number = Math.floor(Math.random() * 10)
+    return number
+    }
+
+//appending the dynamical created div to the html in the location (holder)
+//document.body.holder.appendChild(holder)
